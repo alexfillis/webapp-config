80b3da8035a418da43f3c5e7e0b19ba1414f1238
Alex Fillis <alexfillis@gmail.com>
2015-11-21 13:50:48 +0800

    added feature to lookup value from.xml

--- src/main/java/play/java/web/config/servlet/HelloServlet.java
+++ src/main/java/play/java/web/config/servlet/HelloServlet.java
@@ -1,5 +1,8 @@
 package play.java.web.config.servlet;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +13,21 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.write("<h1>Hello Servlet</h1>");
-        out.close();
+        try {
+            Context context = new InitialContext();
+            // Get a handle to the JNDI environment naming context
+            Context env = (Context)context.lookup("java:comp/env");
+            // Get a single value
+            String webmasterEmail = (String)env.lookup("webmasterEmail");
+
+            PrintWriter out = resp.getWriter();
+            try {
+                out.write(String.format("<h1>Hello %s</h1>", webmasterEmail));
+            } finally {
+                out.close();
+            }
+        } catch (NamingException e) {
+            throw new ServletException(e);
+        }
     }
 }
